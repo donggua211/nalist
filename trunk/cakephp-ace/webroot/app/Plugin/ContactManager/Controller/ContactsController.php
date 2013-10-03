9e21f073b89a86b6d9311bffb161c3bd7a08dedc
zhaoyuan211 <zhaoyuan211@5c7d76bc-719b-7e36-9a3a-16f4029c1fe3>
2013-10-03 05:28:27 +0000

    -ContactManager

---
+++ trunk/cakephp-ace/webroot/app/Plugin/ContactManager/Controller/ContactsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('ContactManagerAppController', 'ContactManager.Controller');
+/**
+ * Contacts Controller
+ *
+ */
+class ContactsController extends ContactManagerAppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ trunk/cakephp-ace/webroot/app/Plugin/ContactManager/Test/Case/Controller/ContactsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('ContactsController', 'ContactManager.Controller');
+
+/**
+ * ContactsController Test Case
+ *
+ */
+class ContactsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.contact_manager.contact'
+	);
+
+}
