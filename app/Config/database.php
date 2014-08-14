953dbfab73c11b5a843685ae4358a95135a54b4d
zhaoyuan211 <zhaoyuan211@5c7d76bc-719b-7e36-9a3a-16f4029c1fe3>
2014-08-14 05:49:54 +0000

    -Update.

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'nalist_ace',
+		'database' => 'nalist_cake',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
