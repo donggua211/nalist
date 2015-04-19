394e8a3823db60c3c893dddf3ca76333f3179c08
Kevin Zhao <donggua211@qq.com>
2015-04-18 17:10:30 -0700

    -Enkj DB setting.

--- application/config/database.php
+++ application/config/database.php
@@ -64,7 +64,7 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
-$db['enkj']['hostname'] = 'localhost';
+$db['enkj']['hostname'] = 'enkj';
 $db['enkj']['username'] = 'a0122091853';
 $db['enkj']['password'] = '98389573';
 $db['enkj']['database'] = 'a0122091853';
